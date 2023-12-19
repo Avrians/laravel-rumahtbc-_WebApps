8acace2f02246d62700fcdb90ffcc44ded35c572
avrians <avriansyahb10@gmail.com>
2023-12-19 09:18:59 +0700

    update

---
+++ app/Http/Controllers/DokterController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dokter;
+use Illuminate\Http\Request;
+
+class DokterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Dokter  $dokter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Dokter $dokter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Dokter  $dokter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Dokter $dokter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Dokter  $dokter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Dokter $dokter)
+    {
+        //
+    }
+}
